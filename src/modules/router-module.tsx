86fc21acef2d66e8de1810c84365d0a771d37b8e
Charles-François St-Cyr <charlesf2001@gmail.com>
2023-12-04 18:44:07 +0100

    Refactor router-module.tsx

--- src/modules/router-module.tsx
+++ src/modules/router-module.tsx
@@ -2,17 +2,18 @@ import React from "react";
 import { Navigate, createBrowserRouter } from "react-router-dom";
 import { SearchPage } from "../pages/search-page/search-page";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <Navigate to="/search" />,
+        },
+        {
+            path: "/search",
+            element: <SearchPage />,
+        },
+    ],
     {
-        path: "/",
-        element: <Navigate to="/search" />,
+        basename: "/Projet_WS",
     },
-    {
-        path: "/Projet_WS",
-        element: <Navigate to="/search" />,
-    },
-    {
-        path: "/search",
-        element: <SearchPage />,
-    },
-]);
+);
