738c63cebcc5017635253fc1bf5234e55db6c1d8
Charles-François St-Cyr <charlesf2001@gmail.com>
2023-12-06 11:22:55 +0100

    set element in createBrowserRouter (#6)

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { FC } from "react";
-import { RouterProvider } from "react-router-dom";
+import { Outlet, RouterProvider } from "react-router-dom";
 import { router } from "../modules/router-module";
 import { Header } from "../components/header/header";
 import "./app.scss";
+import { AppProvider } from "../modules/app-provider";
 
 export const App: FC = () => {
     return (
-        <div className="app">
-            <Header />
-            <RouterProvider router={router} />
-        </div>
+        <AppProvider>
+            <div className="app">
+                <Header />
+                <Outlet />
+            </div>
+        </AppProvider>
     );
 };
--- src/modules/router-module.tsx
+++ src/modules/router-module.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { Navigate, createBrowserRouter } from "react-router-dom";
 import { SearchPage } from "../pages/search-page/search-page";
+import { App } from "../app/app";
 
 export const router = createBrowserRouter(
     [
         {
-            path: "/",
-            element: <Navigate to="/search" />,
-        },
-        {
-            path: "/search",
-            element: <SearchPage />,
+            element: <App />,
+            children: [
+                {
+                    path: "/",
+                    element: <Navigate to="/search" />,
+                },
+                {
+                    path: "/search",
+                    element: <SearchPage />,
+                },
+            ],
         },
     ],
     {
