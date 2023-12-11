7b18bb0e58f23d9fa794c4ddb89acf8ed4257f87
clemgi0 <clement.giraudon2@gmail.com>
2023-12-11 09:41:54 +0100

    finish styling

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -7,9 +7,15 @@ export const Footer: FC = () => {
         <footer>
             <span>{/* TITLE */}</span>
             <nav>
-                <Link to="/search">Search</Link>
-                <Link to="/credit">Credit</Link>
-                <Link to="https://github.com/Septanome/Projet_WS">Source Code</Link>
+                <div className="internal-links">
+                    <Link to="/search">Search</Link>
+                    <Link to="/credit">Credit</Link>
+                </div>
+                <div className="external-links">
+                    <Link to="https://github.com/Septanome/Projet_WS">
+                        <img src="github_logo.png"></img>
+                    </Link>
+                </div>
             </nav>
         </footer>
     );
