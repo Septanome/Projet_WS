1934288baa57fc5789eb0144eeaef0966e707740
clemgi0 <clement.giraudon2@gmail.com>
2023-12-19 21:26:49 +0100

    make better request

--- src/models/request/location-request.ts
+++ src/models/request/location-request.ts
@@ -17,24 +17,40 @@ export class LocationRequest extends Request<LocationResult> {
     formatResult(data: any[]): LocationResult {
         const [resources] = data;
 
+        console.log(resources);
+
+        const binding = resources.results.bindings[0];
+
         return {
-            url: resources.resources.binding.resource.value,
-            label: resources.resources.binding.label.value,
-            description: resources.resources.binding.abstract.value,
-            uriPart: resources.resources.binding.resource.value,
-            thumbnail: resources.resources.binding.thumbnail
-                ? resources.resources.binding.thumbnail.value
-                : null,
+            url: binding.resource?.value || null,
+            label: binding.label?.value || null,
+            description: binding.abstract?.value || null,
+            uriPart: binding.resource?.value || null,
+            thumbnail: binding.thumbnail?.value || null,
+            name: binding.name?.value || null,
+            comment: binding.comment?.value || null,
+            latitude: binding.latitude?.value || null,
+            longitude: binding.longitude?.value || null,
+            country: binding.country?.value || null,
         };
     }
 
     buildSearchQuery(select: string): string {
         return `
-        SELECT ?abstract ?thumbnail
+        SELECT DISTINCT ?abstract ?thumbnail ?name ?comment ?latitude ?longitude ?country
         WHERE {
-          dbr:${select} dbo:abstract ?abstract.
-          OPTIONAL { dbr:NomDuLieu dbo:thumbnail ?thumbnail. }
-          FILTER (LANG(?abstract) = 'en')
+            dbr:${select} dbo:abstract ?abstract.
+            
+            FILTER (lang(?comment) = 'en')
+            FILTER (lang(?abstract) = 'en')
+
+            OPTIONAL { dbr:${select} dbp:label ?label. }
+            OPTIONAL { dbr:${select} dbo:thumbnail ?thumbnail. }
+            OPTIONAL { dbr:${select} dbp:name ?name. }
+            OPTIONAL { dbr:${select} rdfs:comment ?comment. }
+            OPTIONAL { dbr:${select} geo:lat ?latitude. }
+            OPTIONAL { dbr:${select} geo:long ?longitude. }
+            OPTIONAL { dbr:${select} dbo:country ?country. }
         }
         `;
     }
